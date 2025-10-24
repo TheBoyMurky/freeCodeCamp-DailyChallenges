c7f8922363e5986511e2a5b35bdb5ed50cd9eb68
Murky Murloc <murkyboy@protonmail.com>
2025-10-23 21:56:50 -0300

    Solved 15-10-2025

---
+++ HTMLTagStripper 15-10-2025/htmltagstripper.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func stripTags(html string) string {
+	r, _ := regexp.Compile("</?([^<]+)>")
+	strippedTag := r.ReplaceAllString(html, "")
+	return strippedTag
+}
+
+func main() {
+	fmt.Println(stripTags("<a href=\"#\">Click here</a>"))
+	fmt.Println(stripTags("<p><b>Some Text</b></p>"))
+	fmt.Println(stripTags("<p><cite>The Scream</cite> by Edward Munch. Painted in 1893.</p> "))
+}
