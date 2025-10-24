599b2a6dd93a0355e91a7e52f0f504284bf8ee0d
Murky Murloc <murkyboy@protonmail.com>
2025-10-23 22:22:02 -0300

    Resolved 16-10-2025

---
+++ EmailValidator 16-10-2025/emailvalidator.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func emailValidator(email string) bool {
+	r, _ := regexp.Compile("^[^.].+[^.]@(\\w+\\.)+\\w\\w+")
+	return r.MatchString(email)
+}
+
+func main() {
+	fmt.Println(emailValidator("avalid@email.com"))
+	fmt.Println(emailValidator(".notavalid@email.com"))
+	fmt.Println(emailValidator("alsonotavalid@email.c"))
+}
